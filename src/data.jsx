80417b9f6837a43e9d8f88910ee96b0ec61de506
Aravind Ganesh <91867114+Epsilon-Aravind@users.noreply.github.com>
2023-11-21 14:22:03 +0530

    useReducer

--- src/data.jsx
+++ src/data.jsx
@@ -4,28 +4,28 @@ const cartItems = [
     title: 'DELUXE CASSATA',
     price: '60',
     img: './delux-cassata.jpg',
-    amount: 0,
+    amount: 1,
   },
   {
     id: 'recB6qcHPxb62YJ75',
     title: 'Unicone Chocolate',
     price: '50',
     img: './unicone-choclota.jpg',
-    amount: 0,
+    amount: 1,
   },
   {
     id: 'recdRxBsE14Rr2VuJ',
     title: 'Vanilla Sandwich',
     price: '40',
     img: './vanilla-sandwich.jpg',
-    amount: 0,
+    amount: 1,
   },
   {
     id: 'recwTo160XST3PIoW',
     title: 'Big Raspberry',
     price: '30',
     img: './rasberry.jpg',
-    amount: 0,
+    amount: 1,
   },
 ];
 
